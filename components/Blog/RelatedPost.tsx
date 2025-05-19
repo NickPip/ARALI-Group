33d000ba2362da40ca73670f05f5dd1255dab604
NickPip <nikolozpipia98@gmail.com>
2025-05-19 20:06:06 +0400

    last commit

--- components/Blog/RelatedPost.tsx
+++ components/Blog/RelatedPost.tsx
@@ -6,8 +6,9 @@ import useBlogData from "./blogData";
 import { useTranslation } from "react-i18next";
 
 const RelatedPost = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
   const BlogData = useBlogData();
+  const currentLang = i18n.language;
 
   return (
     <>
@@ -17,27 +18,33 @@ const RelatedPost = () => {
         </h4>
 
         <div>
-          {BlogData.slice(0, 3).map((post, key) => (
-            <div
-              className="mb-7.5 flex flex-wrap gap-4 xl:flex-nowrap 2xl:gap-6"
-              key={key}
-            >
-              <div className="relative h-18 w-45 max-w-45">
-                {post.mainImage ? (
-                  <Image fill src={post.mainImage} alt={t(post.title)} />
-                ) : (
-                  t("blog.noImage")
-                )}
+          {BlogData.slice(0, 3).map((post, key) => {
+            const displayTitle =
+              typeof post.title === "object"
+                ? post.title[currentLang as keyof typeof post.title]
+                : post.title;
+            return (
+              <div
+                className="mb-7.5 flex flex-wrap gap-4 xl:flex-nowrap 2xl:gap-6"
+                key={key}
+              >
+                <div className="relative h-18 w-45 max-w-45">
+                  {post.mainImage ? (
+                    <Image fill src={post.mainImage} alt={displayTitle} />
+                  ) : (
+                    t("blog.noImage")
+                  )}
+                </div>
+                <h5 className="text-md font-medium text-black transition-all duration-300 hover:text-primary dark:text-white dark:hover:text-primary">
+                  <Link href={`/blog/blog-details${key === 0 ? "" : key + 1}`}>
+                    {displayTitle.length > 40
+                      ? `${displayTitle.slice(0, 40)}...`
+                      : displayTitle}
+                  </Link>
+                </h5>
               </div>
-              <h5 className="text-md font-medium text-black transition-all duration-300 hover:text-primary dark:text-white dark:hover:text-primary">
-                <Link href={`/blog/blog-details${key === 0 ? "" : key + 1}`}>
-                  {post.title.length > 40
-                    ? `${post.title.slice(0, 40)}...`
-                    : post.title}
-                </Link>
-              </h5>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </div>
     </>
